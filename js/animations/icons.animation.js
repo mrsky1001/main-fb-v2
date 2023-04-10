3efa6a91a539c0c577a94d8aec7a13c3014e027a
mrsky1001 <nikita.nk16@yandex.ru>
2023-04-10 21:09:04 +0600

    before flowbite

--- js/animations/icons.animation.js
+++ js/animations/icons.animation.js
@@ -1,6 +1,6 @@
-Array.from(document.querySelectorAll('.cards > .row > a, .NavItem > a')).forEach(img => {
+Array.from(document.querySelectorAll('.cards > .row > a, .nav-item > a')).forEach(img => {
     const targets = img.querySelectorAll('.path')
-
+    console.log(targets)
     img.onmouseenter = () => {
         anime(animationConfig.onHoverSVG(targets).opts)
     }
