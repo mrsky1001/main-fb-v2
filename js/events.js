5cb925341cfef24a5328d6d2065f71c6e09ea1a0
mrsky1001 <nikita.nk16@yandex.ru>
2023-05-22 21:52:46 +0600

    update

--- js/events.js
+++ js/events.js
@@ -57,6 +57,8 @@ function setFillAllSVG(fill, stroke){
     })
 }
 
+themeContainer.getElementsByClassName("theme-btn")[0].classList.add('show')
+
 function setTheme(theme) {
     const lightSVG = '<path fill-off="#fff" stroke-off="#fff" class="path"  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"></path>'
     const darkSVG = '<path fill-off="#444" stroke-off="#444" class="path" d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>'
@@ -65,19 +67,36 @@ function setTheme(theme) {
 
     if (Number(theme) === themes.dark) {
         const fill ='#fff'
+        setFillAllSVG(fill, fill)
+
         svgEl.setAttribute('fill', fill)
         svgEl.innerHTML = lightSVG
 
-        setFillAllSVG(fill, fill)
+        const pathEl = svgEl.getElementsByClassName("path")[0]
+
+        pathEl.setAttribute('fill-off', fill)
+        pathEl.setAttribute('stroke-off', fill)
+
         document.documentElement.setAttribute('theme', 'dark');
     } else {
-        const fill ='#444'
-        svgEl.setAttribute('fill', fill)
-        svgEl.innerHTML = darkSVG
+        const fill ='#333'
+        const fillBtn =' rgba(51, 51, 51, 0.5)'
 
         setFillAllSVG(fill, fill)
+
+        svgEl.setAttribute('fill', fillBtn)
+        svgEl.innerHTML = darkSVG
+
+
+        const pathEl = svgEl.getElementsByClassName("path")[0]
+
+        pathEl.setAttribute('fill-off', fillBtn)
+        pathEl.setAttribute('stroke-off', fillBtn)
+
+
         document.documentElement.removeAttribute('theme');
     }
+
 }
 
 setTheme(localStorage.getItem('__foma-blog_theme') ?? themes.light)
